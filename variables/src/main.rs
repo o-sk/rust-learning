9af94edcc94e9f8d2d2bfd0b97e052ae3df5370c
o-sk <m.ohsuka@gmail.com>
2019-10-20 11:47:09 +0900

    Learn tupple

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    let tup = (500, 6.4, 1);
+    println!("The values of tup are: ({}, {}, {})", tup.0, tup.1, tup.2);
 }
