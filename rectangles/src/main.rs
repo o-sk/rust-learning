3107d15e845d4b02c86805143630e85687c4038a
o-sk <m.ohsuka@gmail.com>
2019-10-20 22:57:34 +0900

    Use tupple

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,10 +4,22 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area1(width1, height1)
     );
+
+    let rect1 = (30, 50);
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area2(rect1)
+    );
+
 }
 
-fn area(width: u32, height: u32) -> u32 {
+fn area1(width: u32, height: u32) -> u32 {
     width * height
+}
+
+fn area2(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
 }
