bef9e57bd5fb733955661f84b217adb09f58d1e8
o-sk <m.ohsuka@gmail.com>
2019-10-27 19:25:27 +0900

    Learn pub and super

--- communicator/src/lib.rs
+++ communicator/src/lib.rs
@@ -1,11 +1,10 @@
+pub mod client;
+pub mod network;
+
 #[cfg(test)]
-
-mod client;
-mod network;
-
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        super::client::connect();
     }
 }
