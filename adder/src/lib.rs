7c9d038df6d58729c725568d4989c221f6c893c0
o-sk <m.ohsuka@gmail.com>
2019-11-02 23:23:28 +0900

    Add add_two function and unit tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,5 +1,26 @@
 #[cfg(test)]
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
 mod tests {
+    use super::*;
+
+    #[test]
+    fn add_two_and_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn add_three_and_two() {
+        assert_eq!(5, add_two(3));
+    }
+
+    #[test]
+    fn one_hundred() {
+        assert_eq!(102, add_two(100));
+    }
+
     #[test]
     fn exploration() {
         assert_eq!(2 + 2, 4);
