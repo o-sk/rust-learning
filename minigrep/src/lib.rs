ada7f5a5fdeff0a0a0b9e1af875967e5866e592c
o-sk <m.ohsuka@gmail.com>
2019-11-04 11:22:25 +0900

    Add a failing test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -29,3 +29,23 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn one_result() {
+    let query = "duct";
+
+    let contents ="\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+    assert_eq!(
+      vec!["safe, fast, productive."],
+      search(query, contents)
+    );
+  }
+}
