79374922b48dc47c093ff195a82c1a20d18cd7a6
o-sk <m.ohsuka@gmail.com>
2019-11-12 23:23:16 +0900

    Use the move keyword

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -4,7 +4,7 @@ use std::time::Duration;
 fn main() {
     let v = vec![1, 2, 3];
 
-    let handle = thread::spawn(|| {
+    let handle = thread::spawn(move || {
         println!("Here's a vector: {:?}", v);
         for i in 1..10 {
             println!("hi number {} from the spawned thread!", i);
