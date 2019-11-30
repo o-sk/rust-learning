5205a90cbe999b0568372cd21d320fe5c9a622aa
o-sk <m.ohsuka@gmail.com>
2019-11-30 23:18:54 +0900

    Fix typo

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::TcpListener;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -20,9 +22,9 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("GET / HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
     } else {
-        ("GET / HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
     let mut file = File::open(filename).unwrap();
