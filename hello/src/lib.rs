489103a909832aa85267e830373e1c5e12477704
o-sk <m.ohsuka@gmail.com>
2019-12-02 22:59:43 +0900

    Add sender to ThreadPool

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,20 +1,26 @@
+use std::sync::mpsc;
 use std::thread;
 
 pub struct ThreadPool {
-  workers: Vec<Worker>
+  workers: Vec<Worker>,
+  sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
   pub fn new(size: usize) -> ThreadPool {
     assert!(size > 0);
 
+    let (sender, receiver) = mpsc::channel();
     let mut workers = Vec::with_capacity(size);
 
     for id in 0..size {
       workers.push(Worker::new(id));
     }
     ThreadPool {
-      workers
+      workers,
+      sender,
     }
   }
 
