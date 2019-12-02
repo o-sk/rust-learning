5c4123e44f1c4f776b6c3cf22280b7538ed31497
o-sk <m.ohsuka@gmail.com>
2019-12-02 23:05:16 +0900

    Share the receiver among the workers

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,4 +1,6 @@
+use std::sync::Arc;
 use std::sync::mpsc;
+use std::sync::Mutex;
 use std::thread;
 
 pub struct ThreadPool {
@@ -13,10 +15,11 @@ impl ThreadPool {
     assert!(size > 0);
 
     let (sender, receiver) = mpsc::channel();
+    let receiver = Arc::new(Mutex::new(receiver));
     let mut workers = Vec::with_capacity(size);
 
     for id in 0..size {
-      workers.push(Worker::new(id, receiver));
+      workers.push(Worker::new(id, Arc::clone(&receiver)));
     }
     ThreadPool {
       workers,
@@ -38,7 +41,7 @@ struct Worker {
 }
 
 impl Worker {
-  fn new(id: usize, receiver: mpsc::Receiver<Job>) -> Worker {
+  fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
     let thread = thread::spawn(|| {
       receiver;
     });
